edfdb82d72fed944fffd637a43d554fbf5de4829
himakio <93031199+himakio@users.noreply.github.com>
2023-06-02 06:45:18 +0000

    trying

--- livesearch.php
+++ livesearch.php
@@ -1,3 +1,26 @@
 
 <?php
 $xmlDoc=new DOMDocument();
+$xmlDoc->load("xml/All Stocks.xml");
+
+$x=$xmlDoc->getElementsByTagName('link');
+
+//get the q parameter from URL
+$q=$_GET["q"];
+
+$hint="";
+
+//lookup all links from the xml file if length of q>0
+if (strlen($q)>2) {
+	$hint="";
+	for($i=0; $i<($x->length); $i++) {
+	$y=$x->item($i)->getElementsByTagName('title');
+	if ($y->item(0)->nodeType==1) {
+		//find a link matching the search text
+		if (stristr($y->item(0)->childNodes->item(0)->nodeValue,$q)) {
+		if ($hint=="") {
+			$hint=$y->item(0)->childNodes->item(0)->nodeValue;
+		} else {
+			$hint=$hint . "<br />".$y->item(0)->childNodes->item(0)->nodeValue;
+		}
+		}
